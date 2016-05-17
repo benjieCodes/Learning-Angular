6f7fcb8c702f55f2949f834ccde5b82e9d97fb45
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-17 12:00:06 -0400

    working on client side website

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,23 @@
-function config () {
+function config ($stateProvider, $urlRouterProvider) {
 
+  // If a "state" does not exist for the URL
+  // Then go to the page /#/
+  $urlRouterProvider.otherwise('/');
+
+  // Define some application states
+  $stateProvider
+    .state('home', {
+      url: '/'
+    })
+    .state('about', {
+      url: '/about'
+    })
+    .state('contact', {
+      url: '/contact-us'
+    })
+  ;
 }
 
-config.$inject = [];
+config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 export { config };
